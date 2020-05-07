8c083a9b8dc9c094cde6272a06163d3bb586dead
Divya Jaisawal <divyajaisawal@go-jek.com>
2020-05-07 07:26:42 +0530

    Adds mergeksorted ex.

---
+++ src/main/java/com/divya/MergeKSortedList.java
@@ -0,0 +1,40 @@
+package com.divya;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+public class MergeKSortedList<T> {
+
+    public LinkedList mergeKSortedLists(LinkedList<Integer>[] lists) {
+        LinkedList outputList = new LinkedList();
+        PriorityQueue<Object> priorityQueue = new PriorityQueue<>();
+        for (LinkedList list : lists) {
+            for (int i = 0; i < list.size(); i++) {
+                priorityQueue.add(list.get(i));
+            }
+        }
+        while (!priorityQueue.isEmpty()) {
+            outputList.add(priorityQueue.poll());
+        }
+        return outputList;
+    }
+
+    public LinkedListNode mergeKSortedCustomLinkedLists(LinkedListNode[] lists) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        for (LinkedListNode list : lists) {
+            while (list != null) {
+                priorityQueue.add(list.data);
+                list = list.node;
+            }
+        }
+        LinkedListNode result = new LinkedListNode(-1);
+        LinkedListNode head = result;
+
+        while (!priorityQueue.isEmpty()) {
+            head.node = new LinkedListNode(priorityQueue.remove());
+            head = head.node;
+        }
+        return result.node;
+    }
+}
---
+++ src/test/java/com/divya/MergeKSortedListTest.java
@@ -0,0 +1,35 @@
+package com.divya;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+public class MergeKSortedListTest {
+    private MergeKSortedList mergeKSortedList;
+
+    @Before
+    public void setUp() {
+        mergeKSortedList = new MergeKSortedList();
+    }
+
+    @Test
+    public void shouldMergeKSortedList() {
+        LinkedList list1 = new LinkedList();
+        list1.add(2);
+        list1.add(4);
+        list1.add(8);
+        list1.add(12);
+        LinkedList list2 = new LinkedList();
+        list2.add(1);
+        list2.add(5);
+        list2.add(9);
+        list2.add(16);
+
+        LinkedList[] list = new LinkedList[]{list1, list2};
+        LinkedList<Integer> linkedList = mergeKSortedList.mergeKSortedLists(list);
+        for (int n : linkedList){
+            System.out.println(n);
+        }
+    }
+}
